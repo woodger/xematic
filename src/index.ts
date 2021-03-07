e35c271313d8bbbf8a9589dddd9025cf98c79c45
Stanislav Potemkin <woodger@ya.ru>
2021-03-07 13:18:44 +0300

    Associated Schema Type

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,51 @@ export {
   SchemaDefinition
 };
 
+interface IMap {
+  [key: string]: any;
+}
+
+function associatedSchemaType({ type, format }: Definition): any {
+  if (type === 'string' && format === 'date-time') {
+    return Date;
+  }
+
+  if (type === 'string') {
+    return String;
+  }
+
+  if (type === 'number') {
+    return Number;
+  }
+
+  if (type === 'boolean') {
+    return Boolean;
+  }
+
+  if (type === 'array') {
+    return [];
+  }
+
+  throw new Error(`Unxpected type`);
+}
+
 export default function build(definition: Definition): SchemaDefinition {
-  const schema = {};
+  const schema: IMap = {};
 
   if (definition.type === 'object') {
+    if (!definition.properties) {
+      throw new Error(`Missing definition 'properties'`);
+    }
 
+    for (const key of Object.keys(definition.properties)) {
+      const value = definition.properties[key];
+
+      if (typeof value === 'boolean') {
+        throw new Error(`Expected 'properties' as Deffinition type`);
+      }
+
+      schema[key] = associatedSchemaType(value);
+    }
   }
 
   return schema;
